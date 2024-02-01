22c2b83b1c9ea987660cf7825c3eef3e628a09e9
Prakash Chandra Mourya <52892617+pcmourya@users.noreply.github.com>
2024-02-01 14:45:05 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: "https://mern-chat-live1.vercel.app/",
+  origin: "https://mern-chat-live1.vercel.app",
 }))
 
 async function getUserDataFromRequest(req) {
