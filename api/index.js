7f1e9337b5684cfae82f59d2d006eb528b8559da
Prakash Chandra Mourya <52892617+pcmourya@users.noreply.github.com>
2024-02-01 14:41:55 +0530

    url changes for cors.

--- api/index.js
+++ api/index.js
@@ -21,7 +21,10 @@ const app = express();
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: "https://mern-chat-live1.vercel.app/",
+}))
 
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
